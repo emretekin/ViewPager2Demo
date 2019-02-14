103fbc6357566ae62521e6a2442340d0945cf452
emretekin <emretekin777@hotmail.com>
2019-02-15 02:30:03 +0300

    First Commit

---
+++ app/src/main/java/com/emretekin/viewpager2demo/ActivityMain.kt
@@ -0,0 +1,49 @@
+package com.emretekin.viewpager2demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+
+class ActivityMain : AppCompatActivity() {
+
+    lateinit var viewPager : ViewPager2
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        createViews()
+        setViewPager()
+    }
+
+    fun createViews(){
+        viewPager = findViewById(R.id.view_pager)
+    }
+
+    fun setViewPager(){
+        viewPager.adapter = BeerPagerAdapter(Beers.BeersStrings)
+        viewPager.orientation = ViewPager2.ORIENTATION_VERTICAL
+        //viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+    }
+
+    class BeerPagerAdapter(private val cheeseStrings: Array<String>) : RecyclerView.Adapter<BeerViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BeerViewHolder {
+            return BeerViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.row_beer, parent, false))
+        }
+
+        override fun onBindViewHolder(holder: BeerViewHolder, position: Int) {
+            holder.beerName.text = cheeseStrings[position]
+        }
+
+        override fun getItemCount() = cheeseStrings.size
+    }
+
+    class BeerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val beerName: TextView = view.findViewById(R.id.beer_name)
+    }
+}
---
+++ app/src/main/java/com/emretekin/viewpager2demo/Beers.kt
@@ -0,0 +1,7 @@
+package com.emretekin.viewpager2demo
+
+object Beers {
+    val BeersStrings = arrayOf(
+        "Coors", "Corona", "Yanjing", "Harbin", "Heineken", "Skol", "Bud Light", "Tsingtao", "Budweiser", "Snow"
+    )
+}
